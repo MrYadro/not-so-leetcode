602ae31e59ec5906a04b9f1623c319cf40b7322d
Y.Yadrushnikov <me@yay.qa>
2022-10-29 17:37:40 +0300

    add some stupid cases, might speed up a bit

--- 383_ransom_note.go
+++ 383_ransom_note.go
@@ -1,17 +1,28 @@
 package leetcode
 
 // https://leetcode.com/problems/ransom-note/
-// Runtime: 34 ms
+// Runtime: 16 ms
 // Memory Usage: 3.9 MB
 
 func CanConstruct(ransomNote string, magazine string) bool {
+	lenMag := len(magazine)
+	lenRN := len(ransomNote)
+
+	if ransomNote == "" {
+		return true
+	}
+
+	if lenMag < lenRN {
+		return false
+	}
+
 	m := make(map[rune]int)
 
-	for i := 0; i < len(magazine); i++ {
+	for i := 0; i < lenMag; i++ {
 		m[rune(magazine[i])]++
 	}
 
-	for i := 0; i < len(ransomNote); i++ {
+	for i := 0; i < lenRN; i++ {
 		if m[rune(ransomNote[i])] == 0 {
 			return false
 		}
